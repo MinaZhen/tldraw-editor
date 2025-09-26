c8a960190c92aaa52630adbce4048ac170bc315a
Mina Zhen <minazhen.mclares@gmail.com>
2025-09-26 08:40:53 +0000

    chore: dependencies install and clean up

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,24 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    rules: {
+      "max-len": ["error", { code: 100, "ignoreComments": true }], 
+      "no-console": "warn",
+      "indent": ["error", 2], 
+      "quotes": ["error", "double", { avoidEscape: true }], 
+      "comma-dangle": ["error", "always-multiline"], 
+      "object-curly-spacing": ["error", "always"], 
+      "semi": ["error", "always"], 
+      "@typescript-eslint/no-explicit-any": "warn", 
+      "@typescript-eslint/explicit-module-boundary-types": "off", 
+      "@typescript-eslint/no-unused-vars": ["warn", { 
+        argsIgnorePattern: "^_", 
+        varsIgnorePattern: "^_", 
+        caughtErrorsIgnorePattern: "^_", 
+      }], 
+    },
+  }),
   {
     ignores: [
       "node_modules/**",
